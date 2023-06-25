f4ee5664d899faf61c44acef4a52bb6815f5a88d
Daniel Waltz <danielbwaltz@gmail.com>
2023-06-25 09:01:04 -0400

    feat: enable experimental no scripts option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config/
 export default defineNuxtConfig({
   modules: ['@unocss/nuxt'],
+  experimental: {
+    noScripts: true,
+  },
   typescript: {
     shim: false,
   },
