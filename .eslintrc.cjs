5cd6ea40a5f5d0a154354ba51ac2fd929e196582
Daniel Waltz <danielbwaltz@gmail.com>
2023-06-25 09:01:03 -0400

    feat: switch to nuxt 3

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+};
