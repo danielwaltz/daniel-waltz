9bf75faf83b5826e7bfd3923973c9df17cbc057e
Daniel Waltz <danielbwaltz@gmail.com>
2024-05-02 21:16:01 -0400

    chore: simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,4 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 import unocss from '@unocss/eslint-config/flat';
 import prettier from 'eslint-plugin-prettier/recommended';
 
-export default withNuxt().append([unocss, prettier]);
+export default withNuxt([unocss, prettier]);
