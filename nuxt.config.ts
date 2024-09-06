36d0eed46c6b0ffc3ded199522b2e9dda072954a
Daniel Waltz <danielbwaltz@gmail.com>
2024-09-06 16:18:31 -0400

    chore: update deps and bump compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config/
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-23',
+  compatibilityDate: '2024-09-06',
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
