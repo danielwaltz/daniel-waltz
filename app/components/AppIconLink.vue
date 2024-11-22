305aafc06a4589c257e5f35267ea57de0c185e03
Daniel Waltz <danielbwaltz@gmail.com>
2024-11-21 19:47:12 -0500

    feat: add title to app icon links

--- app/components/AppIconLink.vue
+++ app/components/AppIconLink.vue
@@ -9,7 +9,12 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="to" rel="noopener noreferrer" class="inline-flex">
+  <NuxtLink
+    :to="to"
+    :title="title"
+    rel="noopener noreferrer"
+    class="inline-flex"
+  >
     <i :class="icon" />
     <span class="sr-only">{{ title }}</span>
   </NuxtLink>
