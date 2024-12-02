a4d1e6506857ec261eb29f39fa4f9b0bc3490af3
Daniel Waltz <danielbwaltz@gmail.com>
2024-12-01 21:59:29 -0500

    refactor: use node process for healthcheck uptime

--- server/routes/health.get.ts
+++ server/routes/health.get.ts
@@ -1,6 +1,4 @@
-const START_TIME = new Date();
-
 export default defineEventHandler(() => ({
   status: "success",
-  uptime: Math.floor((Date.now() - START_TIME.getTime()) / 1000),
+  uptime: Math.floor(process.uptime()),
 }));
