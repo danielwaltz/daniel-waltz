4d4b481352a0937ecef2f998b0f45bfa52c3ed07
Daniel Waltz <danielbwaltz@gmail.com>
2025-01-25 18:08:03 -0500

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,5 @@
 import { danielwaltz } from "@danielwaltz/eslint-config";
 import { withNuxt } from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(danielwaltz().toConfigs());
+// @ts-expect-error `@nuxt/eslint` needs newest `eslint-flat-config-utils` version
+export default withNuxt(danielwaltz());
