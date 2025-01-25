d51be3df04e41f995e5ebe55019d5da86105fcf5
Daniel Waltz <danielbwaltz@gmail.com>
2025-01-25 09:19:42 -0500

    chore: update deps

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
 import { danielwaltz } from "@danielwaltz/eslint-config";
 import { withNuxt } from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(danielwaltz());
+export default withNuxt(danielwaltz().toConfigs());
