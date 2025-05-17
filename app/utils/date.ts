5c96322fe2a5a9064f5d5710a4872e4bc953a963
Daniel Waltz <danielbwaltz@gmail.com>
2025-05-17 07:42:54 -0400

    chore: remove unused date format util

--- app/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date: number | string | Date): string => {
-  return new Date(date).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-};
