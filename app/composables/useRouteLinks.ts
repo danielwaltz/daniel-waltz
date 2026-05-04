b4b38fa50365ac9c5d7b2118934d3c68dd6eef35
Daniel Waltz <danielbwaltz@gmail.com>
2026-05-04 13:50:14 -0400

    chore: improve use route links type safety

--- app/composables/useRouteLinks.ts
+++ app/composables/useRouteLinks.ts
@@ -1,32 +1,22 @@
-import type { NuxtLinkProps } from "#app";
-import type { RouteLocationTyped } from "vue-router";
+import type {
+  RouteLocationAsRelative,
+  RouteLocationResolved,
+} from "vue-router";
 import type { RouteNamedMap } from "vue-router/auto-routes";
 
-export type RouteLinkItem<TName extends keyof RouteNamedMap> = Omit<
-  NuxtLinkProps,
-  "to" | "href"
-> & {
-  name: TName;
-  to?: Omit<Partial<RouteLocationTyped<RouteNamedMap, TName>>, "name">;
-};
-
-export function useRouteLinks<TItem extends RouteLinkItem<keyof RouteNamedMap>>(
-  items: TItem[],
-) {
+export function useRouteLinks<
+  const TItems extends RouteLocationAsRelative<keyof RouteNamedMap>[],
+>(items: TItems) {
   const router = useRouter();
   const route = useRoute();
 
   return computed(() => {
     return items.map((item) => {
-      const { name: _name, ...props } = item;
-      const name = _name as TItem["name"];
-      const resolvedRoute = router.resolve({ name }, route);
-
-      return {
-        ...resolvedRoute,
-        name,
-        props: { ...props, to: { ...props.to, name } },
-      };
+      type Name = NonNullable<TItems[number]["name"]>;
+      const name = item.name as Name;
+      const _resolvedRoute = router.resolve({ name }, route);
+      const resolvedRoute = _resolvedRoute as RouteLocationResolved<Name>;
+      return { ...resolvedRoute, props: { to: item } } as const;
     });
   });
 }
