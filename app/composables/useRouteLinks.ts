fd9fe3e9f943e2fff4fae974d096e8247d0bff29
Daniel Waltz <danielbwaltz@gmail.com>
2026-05-05 16:27:04 -0400

    chore: update deps

--- app/composables/useRouteLinks.ts
+++ app/composables/useRouteLinks.ts
@@ -5,14 +5,14 @@ import type {
 import type { RouteNamedMap } from "vue-router/auto-routes";
 
 export function useRouteLinks<
-  const TItems extends RouteLocationAsRelative<keyof RouteNamedMap>[],
->(items: TItems) {
+  TItem extends RouteLocationAsRelative<keyof RouteNamedMap>,
+>(items: TItem[]) {
   const router = useRouter();
   const route = useRoute();
 
   return computed(() => {
     return items.map((item) => {
-      type Name = NonNullable<TItems[number]["name"]>;
+      type Name = NonNullable<TItem["name"]>;
       const name = item.name as Name;
       const _resolvedRoute = router.resolve({ name }, route);
       const resolvedRoute = _resolvedRoute as RouteLocationResolved<Name>;
