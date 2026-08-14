456d3d059a755a0d9bcc0db57d219b1af0773658
Daniel Waltz <danielbwaltz@gmail.com>
2026-08-13 22:36:35 -0400

    feat: update skip link styling

--- app/components/AppSkip.vue
+++ app/components/AppSkip.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     href="#content"
-    class="text-neutral-100 px-1 rounded bg-neutral-800 opacity-0 decoration-none pointer-events-none translate-x--50% transition inset-bs-2 inset-is-50% absolute focus:opacity-100 focus:pointer-events-auto"
+    class="text-neutral-100 font-semibold px-1 rounded bg-neutral-800 opacity-0 decoration-none pointer-events-none translate-x--50% transition inset-bs-2 inset-is-50% absolute z-1 focus:opacity-100 focus:pointer-events-auto"
   >
     Skip to content
   </a>
